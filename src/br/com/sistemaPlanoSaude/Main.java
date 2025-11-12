4e5e01bf9e97576a62082e5513050c8b3a6c91b3
KauzCoder <kauzcoder.dev@gmail.com>
2025-11-11 22:38:42 -0300

    Apaguei o teste na Main

--- src/br/com/sistemaPlanoSaude/Main.java
+++
@@ -1,8 +0,0 @@
-package br.com.sistemaPlanoSaude;
-import br.com.sistemaPlanoSaude.*;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bem-vindo ao Sistema de Plano de Saúde!");
-        // Aqui você pode adicionar a lógica principal do sistema
-    }
-}
